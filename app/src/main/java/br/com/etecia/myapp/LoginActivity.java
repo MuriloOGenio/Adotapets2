37832e3346a109faa148c73b72e8f8070eeae8f2
MuriloOGenio <mueugi@gmail.com>
2023-05-18 19:20:43 -0300

    terminando janela de login e começando cadastro usuário

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,9 +2,12 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -24,6 +27,35 @@ public class LoginActivity extends AppCompatActivity {
         txtSenha = findViewById(R.id.txtSenha);
 
         lblCadastrarUsuario = findViewById(R.id.lblCadastrarUsuario);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        lblCadastrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CadUsuActivity.class));
+            }
+        });
 
     }
 }
